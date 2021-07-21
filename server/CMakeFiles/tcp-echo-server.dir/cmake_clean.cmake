e2f54b5860a4c65432f50f1b36bcf4cfc790e3c9
Daria K <dorykolesnikova@gmail.com>
2021-07-21 18:12:30 +0200

    server

---
+++ server/CMakeFiles/tcp-echo-server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tcp-echo-server.dir/Server.cpp.o"
+  "tcp-echo-server"
+  "tcp-echo-server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tcp-echo-server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
